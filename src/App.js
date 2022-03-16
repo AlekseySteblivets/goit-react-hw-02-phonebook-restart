3e4641f4252faa99cb7f210f5c6a2c18fcb895f1
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2022-03-16 16:45:48 +0200

    добавил имя, номер тел и фильтр в APP

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import { v4 as uuidv4 } from 'uuid';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
     number: ''
   }
@@ -43,7 +49,24 @@ class App extends Component {
     }));
   }
 
+  // handleFilterChange = (evt) => {
+  //   evt.preventDefault();
+  //   this.setState({
+  //     filter: evt.currentTarget.value
+  //   })
+  // }
+
+  getVisibleContact = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact => contact.name.includes(normalizedFilter));
+
+  }
+
+
   render() {
+    const visibleContacts = this.getVisibleContact();
     return (
       <>
         <h1>Phonebook</h1>
@@ -75,11 +98,25 @@ class App extends Component {
 
         <div>
           <h2>Contacts</h2>
+          <div>
+            <p>Find contacts by name</p>
+            <input
+              type="text"
+              name="filter"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={this.state.filter}
+              onChange={this.handleInputChange}
+            />
+          </div>
           <ul>
-            {this.state.contacts.map(contact =>
+            {visibleContacts.map(contact =>
               <li key={uuidv4()}>{contact.name}: {contact.number}</li>
             )
             }
+
+
           </ul>
         </div>
       </>
